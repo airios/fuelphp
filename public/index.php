5891e28e09add0daed3e6db2f7831c327bee04b8
Dan Horrigan <dan@dhorrigan.com>
2012-08-05 17:23:57 -0400

    Adding the whitelist check to index.php

--- public/index.php
+++ public/index.php
@@ -35,6 +35,21 @@ $env->init(array(
 $app = $env->loader->loadApplication('app', function() {});
 
 /**
+ * Check to make sure the requester's IP is whitelisted or not.  This does not
+ * run the check when in production.
+ *
+ * To enable the whitelist, add the IP addresses in the app's config 'security.ipWhitelist'.
+ *
+ * Note: You may remove this if you like, however, note that it adds no overhead while
+ * in production.
+ */
+if ($env->name != 'production' and ! $app->security->checkIpWhitelist())
+{
+	header('HTTP/1.1 403 Forbidden');
+	exit('403 Forbidden');
+}
+
+/**
  * Run the app and output the response headers
  */
 $response = $app->request($env->input->getPathInfo())->execute()->getResponse()->sendHeaders()->getContent();
